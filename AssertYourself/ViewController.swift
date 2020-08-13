7f77bd320c97b8fe409b7a7f61439de335ccb48a
Quinne Farenwald <quinne.farenwald@slalom.com>
2020-08-13 16:16:47 -0600

    Initial Commit

---
+++ AssertYourself/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AssertYourself
+//
+//  Created by Quinne Farenwald on 8/13/20.
+//  Copyright © 2020 Quinne Farenwald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
