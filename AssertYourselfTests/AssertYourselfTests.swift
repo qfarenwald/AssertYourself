f2d59c8b877e95dd3d261ed4bacea56f25190dd9
Quinne Farenwald <quinne.farenwald@slalom.com>
2020-09-08 19:29:12 -0600

    first commit

--- AssertYourselfTests/AssertYourselfTests.swift
+++ AssertYourselfTests/AssertYourselfTests.swift
@@ -10,25 +10,68 @@ import XCTest
 @testable import AssertYourself
 
 class AssertYourselfTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    func test_fail() {
+        let theAnswer: Int = 42
+        XCTFail("The answer is \(42)!")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_useConditional() {
+        let success = false
+        if !success {
+            XCTFail("Success is \(success)")
         }
     }
-
+    
+    func test_avoidConditional() {
+        let success = true
+        XCTAssertTrue(success)
+    }
+    
+    func test_assertNil() {
+        let optionalValue: Int? = 123
+        XCTAssertNil(optionalValue)
+    }
+    
+    struct SimpleStruct {
+        let x: Int
+        let y: Int
+    }
+    
+    func test_assertNil_SimpleStruct() {
+        let testThing: SimpleStruct = SimpleStruct(x: 1, y: 2)
+        XCTAssertNil(testThing)
+    }
+    
+    struct DescriptStruct: CustomStringConvertible {
+        let x: Int
+        let y: Int
+        
+        var description: String { "YIKES \(x), \(y)" }
+    }
+    
+    func test_assertNil_DescriptStruct() {
+        let testThing: DescriptStruct = DescriptStruct(x: 3, y: 4)
+        XCTAssertNil(testThing)
+    }
+    
+    func test_assertEqual() {
+        let testThing = "testThing"
+        XCTAssertEqual(testThing, "checkingTestThingAgainst")
+    }
+    
+    func test_assertEqualWithOptional() {
+        let testThing: String? = "testThing"
+        XCTAssertEqual(testThing, "checkingTestThingAgainst")
+    }
+    
+    func test_floatingPointDanger() {
+        let result = 0.1 + 0.2
+        XCTAssertEqual(result, 0.3)
+    }
+    
+    func test_floatingPointAccuracyCheck() {
+        let testThing = 0.1 + 0.2
+        XCTAssertEqual(testThing, 0.3, accuracy: 0.0001)
+    }
 }
